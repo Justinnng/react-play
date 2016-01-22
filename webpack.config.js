1bd41c767886947a04e3e2dbf0548fb44d20aa40
Justin Zeng <j.zeng@aquarius-asia.com>
2016-01-22 15:18:26 +0800

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 var config = {
   entry: [
     'webpack/hot/dev-server',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
+    'webpack-dev-server/client?http://localhost:8080',
     "bootstrap-sass!./bootstrap-sass.config.js",
     path.resolve(__dirname, 'app/main.jsx')
   ],
@@ -17,7 +17,6 @@ var config = {
       jQuery: "jquery"
     })
   ],
-  devtool: "source-map",
   module: {
     loaders: [{
       test: /\.jsx?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
@@ -26,10 +25,10 @@ var config = {
       query: {
         plugins: ['transform-runtime'],
         presets: [ 'es2015', 'stage-0', 'react' ]
-        }
+      }
     }, {
       test: /\.scss$/,
-      loader: 'style!css!sass?sourceMap'
+      loader: 'style!css!sass'
     }, {
       test: /\.(png|jpg)$/,
       loader: 'url?limit=25000'
@@ -40,13 +39,7 @@ var config = {
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=images/svg+xml" }]
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
-
+  }
 };
 
 module.exports = config;
